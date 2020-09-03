7d54dad9e2f1c072c012239b0a59564f95e3e674
anliwen <anliwen@126.com>
2020-09-03 18:00:12 +0800

    调整测试代码

--- integration-tests/src/test/java/com/anlw/Test.java
+++ integration-tests/src/test/java/com/anlw/Test.java
@@ -4,7 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
 		HelloIoc hw = (HelloIoc) context.getBean("hw");
 		hw.show();
 	}
